e0fb4c979daed7de64f03b5492e1052802940d00
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-10 16:24:01 -0800

    revised html and began on css styling and imported dom elements

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 // import functions and grab DOM elements
+const cityDropdown = document.getElementById('city-dropdown');
+const countrysideDropdown = document.getElementById('countryside-dropdown');
+const forestDropdown = document.getElementById('forest-dropdown');
+
+const cityEl = document.getElementById('city');
+const countrysideEl = document.getElementById('countryside');
+const forestEl = document.getElementById('forest');
+
+const sloganEl = document.getElementById('slogans');
+const sloganInput = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
+
+const nameEl = document.getElementById('names');
+const nameInput = document.getElementById('name-input');
+const nameButton = document.getElementById('name-button');
 
 // let state
+
+
+
+
+
+
+
+
+
+
+
 
 // set event listeners 
   // get user input
